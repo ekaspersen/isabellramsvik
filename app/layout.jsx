2df8016b81392a12c39e34ec6bf88875969ad528
Eskil K. Hagen <eskilkaspersen@gmail.com>
2025-04-04 18:28:11 +0200

    Wrap scrollhandler in suspense and ensure not.found.js is correct

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,15 +1,17 @@
 import Header from "./components/sections/Header";
-import ScrollHandler from "./components/ScrollHandler"; // Import the new component
+import ScrollHandler from "./components/ScrollHandler";
 import "./globals.css";
+import { Suspense } from "react";
 
 export default function RootLayout({ children }) {
     return (
         <html lang="no">
             <body>
                 <Header />
-                <ScrollHandler />
+                <Suspense fallback={<div>Loading...</div>}>
+                    <ScrollHandler />
+                </Suspense>
                 {children}
-                {/* Footer can be added here */}
             </body>
         </html>
     );
